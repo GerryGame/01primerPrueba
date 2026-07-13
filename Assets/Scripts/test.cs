c6a39d5d33693854bea1e6f6a51282fd51804fae
GerryGame <jp4037049@gmail.com>
2026-07-12 20:21:34 -0600

    Update test.cs
    En el script test se han mandado mensajes que permiten ir creando mensajes dependiendo sea el caso a la hora de programar.. informativo, advertencia y error

--- Assets/Scripts/test.cs
+++ Assets/Scripts/test.cs
@@ -7,7 +7,15 @@ public class test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        print("Funcion antigua para mandar un mensaje: print ya que es herada de monoBehavior");
+
+        Debug.Log("Mensaje para ir validando la programacion");
+
+        Debug.LogError("Mensaje de Error en la aplicacion");
+
+        Debug.LogWarning("Dejar un mensaje de advertencia");
+
     }
 
     // Update is called once per frame
